1bc1283a3ef26105eed80fb8160f9b64ef3f2d80
Mihirlalwani <mihirlalwani28@gmail.com>
2020-12-12 00:47:11 +0530

    WebTutplus Tutorial

---
+++ src/main/java/tutorial/user/User.java
@@ -0,0 +1,85 @@
+package tutorial.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+@Entity
+@Table(name = "users")
+public class User {
+    private @Id @GeneratedValue long id;
+    private @NotBlank String username;
+    private String email;
+    private @NotBlank String password;
+    private @NotBlank boolean loggedIn;
+    public User() {
+    }
+    public User(@NotBlank String username,
+                String email,
+                @NotBlank String password) {
+        this.username = username;
+       this.email=email;
+        this.password = password;
+        this.loggedIn = false;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof User))
+            return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) && Objects.equals(email,user.email) &&
+                Objects.equals(password, user.password);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password,email,
+                loggedIn);
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", loggedIn=" + loggedIn +
+                '}';
+    }
+}
